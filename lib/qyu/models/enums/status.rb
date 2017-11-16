f99dc48d1e0b276f1dfa779391a47eae60380ac7
oss92 <mohamed.o.alnagdy@gmail.com>
2017-11-16 17:46:50 +0100

    Add models

---
+++ lib/qyu/models/enums/status.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Qyu
+  class Status
+    COMPLETED       = 'completed'
+    QUEUED          = 'queued'
+    WORKING         = 'working'
+    FAILED          = 'failed'
+    INVALID_PAYLOAD = 'invalid_payload'
+
+    def self.find(id)
+      new(id)
+    end
+
+    def initialize(id)
+      @id = id
+    end
+
+    def status
+      t = Qyu.store.find_task(@id)
+      t['status']
+    end
+
+    def completed?
+      status == COMPLETED
+    end
+
+    def queued?
+      status == QUEUED
+    end
+
+    def working?
+      status == WORKING
+    end
+
+    def failed?
+      status == FAILED
+    end
+
+    def invalid_payload?
+      status == INVALID_PAYLOAD
+    end
+  end
+end

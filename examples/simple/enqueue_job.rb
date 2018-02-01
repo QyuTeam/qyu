c236002d9a0e4996817e1907c46706110a24309c
oss92 <mohamed.o.alnagdy@gmail.com>
2018-02-01 17:10:02 +0100

    Fix example

--- examples/simple/enqueue_job.rb
+++ examples/simple/enqueue_job.rb
@@ -2,7 +2,7 @@
 
 require_relative '../config'
 
-payload = { times: 5 }
+payload = { 'times' => 5 }
 Qyu.logger.info "Enqueuing job with payload #{payload}"
 job = Qyu::Job.create(workflow: 'say-hello', payload: payload)
 job.start

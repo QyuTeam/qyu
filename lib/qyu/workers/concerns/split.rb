e3acc381c711aa0728cc326635c0127097fd6f51
oss92 <mohamed.o.alnagdy@gmail.com>
2018-03-27 23:36:54 +0200

    [qyu-1] add split logic concern Qyu::Workers::Concerns::Split

---
+++ lib/qyu/workers/concerns/split.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Qyu
+  module Workers
+    module Concerns
+      # Qyu::Workers::Concerns::Split
+      #
+      # Adds ability to split workers to specify slice size and splittable variable name
+      #
+      # Qyu::SplitWorker.new do
+      #   slice_size 25
+      #   payload_key 'array'
+      # end
+      #
+      module Split
+
+        @slice_size = 25
+
+        # Configures slice size
+        #
+        #   slice_size 25
+        #
+        # @param slice_size [Integer]
+        def slice_size(slsz)
+          @slice_size = slsz
+        end
+
+        # Configures payload key with array to split
+        #
+        #   payload_key 25
+        #
+        # @param payload_key [String]
+        def payload_key(var_name)
+          @payload_key = var_name
+        end
+      end
+    end
+  end
+end

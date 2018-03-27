bb092c8504b6f405236fbf4ea6084e5fbe35c2f0
oss92 <mohamed.o.alnagdy@gmail.com>
2018-03-27 23:37:34 +0200

    [qyu-1] finalize split worker logic

--- lib/qyu/workers.rb
+++ lib/qyu/workers.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-require 'qyu/workers/concerns/callback'
-require 'qyu/workers/concerns/failure_queue'
-require 'qyu/workers/concerns/payload_validator'
-require 'qyu/workers/base'
-require 'qyu/workers/split'
-require 'qyu/workers/sync'
+(
+  Dir["#{File.dirname(__FILE__)}/workers/concerns/*.rb"] +
+  Dir["#{File.dirname(__FILE__)}/workers/*.rb"]
+).each do |path|
+  require path
+end
+
 
 Qyu::Worker = Qyu::Workers::Base
 Qyu::SplitWorker = Qyu::Workers::Split

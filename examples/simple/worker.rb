25e51a6742c4f60746f4a9a98f2a05b2ccc54a07
oss92 <mohamed.o.alnagdy@gmail.com>
2018-03-28 02:20:58 +0200

    [qyu-1] neater example workers
    Signed-off-by: oss92 <mohamed.o.alnagdy@gmail.com>

--- examples/simple/worker.rb
+++ examples/simple/worker.rb
@@ -3,8 +3,8 @@
 require_relative '../config'
 
 class SimpleWorker
-  def initialize
-    @worker = Qyu::Worker.new do
+  def run
+    Qyu::Worker.new do
       callback :execute, :before do
         Qyu.logger.info 'Waiting for task..'
       end
@@ -15,18 +15,18 @@ class SimpleWorker
 
       # Payload validation
       validates :times, presence: true, type: :integer
-    end
-  end
 
-  def run
-    # Consumes message from print-hello queue
-    @worker.work('print-hello') do |task|
-      task.payload['times'].times do |i|
-        Qyu.logger.info "#{i + 1}. Hello"
+      # Consumes messages from print-hello queue
+      work('print-hello') do |task|
+        begin
+          task.payload['times'].times do |i|
+            Qyu.logger.info "#{i + 1}. Hello"
+          end
+        rescue StandardError => ex
+          Qyu.logger.error 'OMG :('
+          Qyu.logger.error ex.message
+        end
       end
-    rescue StandardError => ex
-      Qyu.logger.error 'OMG :('
-      Qyu.logger.error ex.message
     end
   end
 end
